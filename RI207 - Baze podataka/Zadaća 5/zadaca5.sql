27ede7e00d5c81b09a968662c176094f239a005f
Elnur Bjelić <elnur.bjelic1337@gmail.com>
2023-11-13 16:05:15 +0100

    Dodano prvih 11 zadataka zadaće 5

--- RI207 - Baze podataka/Zadaća 5/zadaca5.sql
+++ RI207 - Baze podataka/Zadaća 5/zadaca5.sql
@@ -24,41 +24,56 @@ WHERE
                                                         (MONTHNAME(datIspit) = "January" OR MONTHNAME(datIspit) = "February"));
                                                         
 SELECT imeStud, prezStud, datRodStud FROM stud
-WHERE DATE_ADD(datRodStud, INTERVAL -200 DAY) >= (SELECT MIN(datRodStud) FROM stud);
+WHERE DATE_ADD(datRodStud, INTERVAL 200 DAY) <= (SELECT MAX(datRodStud) FROM stud);
 
-SELECT * FROM pred WHERE sifOrgjed IN
-(SELECT sifOrgjed FROM nastavnik WHERE prezNastavnik LIKE 'O%'); 
+SELECT * FROM pred
+WHERE
+	sifOrgjed IN (SELECT DISTINCT sifOrgjed FROM nastavnik WHERE prezNastavnik LIKE "O%");
+    
+SELECT * FROM dvorana WHERE kapacitet > (SELECT COUNT(sifPred) FROM rezervacija WHERE oznDvorana = dvorana.oznDvorana);
 
-SELECT oznDvorana, kapacitet FROM dvorana
-WHERE kapacitet > (
-SELECT COUNT(sifPred) FROM rezervacija WHERE oznDvorana = dvorana.oznDvorana); 
+SELECT
+	nastavnik.sifNastavnik,
+    nastavnik.imeNastavnik,
+    nastavnik.prezNastavnik,
+    nastavnik.koef,
+    mjesto.nazMjesto
+FROM nastavnik
+JOIN mjesto ON nastavnik.pbrStan = mjesto.pbr
+WHERE koef < (SELECT AVG(koef) FROM nastavnik
+			  JOIN mjesto AS mjesto2 ON nastavnik.pbrStan = mjesto2.pbr
+              WHERE mjesto.sifZupanija = mjesto2.sifZupanija
+			 );
+             
+SELECT
+	stud.mbrStud,
+    stud.imeStud,
+    stud.prezStud
+FROM stud
+WHERE DAYNAME(stud.datRodStud) NOT IN (SELECT DAYNAME(ispit.datIspit) FROM ispit WHERE ispit.mbrStud = stud.mbrStud);
 
-SELECT sifnastavnik, imenastavnik, preznastavnik, koef, nazmjesto
-FROM nastavnik INNER JOIN mjesto ON nastavnik.pbrstan = mjesto.pbr
-WHERE koef < (
-SELECT AVG(koef) FROM nastavnik Ostali INNER JOIN mjesto DrugoM ON
-Ostali.pbrstan = DrugoM.pbr
-WHERE mjesto.sifZupanija=DrugoM.sifZupanija
-AND Ostali.sifNastavnik<>nastavnik.sifNastavnik); 
+SELECT 
+	nastavnik.sifNastavnik,
+    nastavnik.imeNastavnik,
+    nastavnik.prezNastavnik
+FROM nastavnik
+JOIN mjesto ON nastavnik.pbrStan = mjesto.pbr
+JOIN zupanija ON mjesto.sifZupanija = zupanija.sifZupanija
+WHERE
+	zupanija.nazZupanija = "Dubrovačko-neretvanska"
+    AND
+    nastavnik.koef > ANY (SELECT nastavnik.koef FROM nastavnik
+						  JOIN mjesto ON nastavnik.pbrStan = mjesto.pbr
+                          JOIN zupanija ON mjesto.sifZupanija = zupanija.sifZupanija
+                          WHERE zupanija.nazZupanija = "Splitsko-dalmatinska");
 
-SELECT mbrstud, imestud, prezstud FROM stud
-WHERE NOT EXISTS (
-SELECT * FROM ispit WHERE ispit.mbrstud = stud.mbrstud AND
-WEEKDAY(ispit.datispit) = WEEKDAY(stud.datrodstud)); 
-
-SELECT sifNastavnik, imeNastavnik, prezNastavnik
-FROM nastavnik INNER JOIN mjesto ON nastavnik.pbrStan = mjesto.pbr
-INNER JOIN zupanija ON mjesto.sifzupanija = zupanija.sifzupanija
-WHERE nazzupanija = "Dubrovačko-neretvanska"
-AND koef > ANY (
-SELECT koef FROM nastavnik INNER JOIN mjesto ON nastavnik.pbrStan = mjesto.pbr
-INNER JOIN zupanija ON mjesto.sifzupanija = zupanija.sifzupanija
-WHERE nazzupanija = "Splitsko-dalmatinska"); 
-
-SELECT ispit.mbrstud, imestud, prezstud
-FROM ispit INNER JOIN stud ON ispit.mbrstud = stud.mbrstud
-WHERE ocjena > 1
-AND NOT EXISTS (SELECT * FROM ispit ispitP
-WHERE ispitP.sifNastavnik = ispit.sifNastavnik AND ocjena > 1
-AND ispit.mbrStud <> ispitP.mbrStud); 
-
+SELECT
+	stud.mbrStud,
+    stud.imeStud,
+    stud.prezStud
+FROM stud
+JOIN ispit ON stud.mbrStud = ispit.mbrStud
+WHERE
+	ispit.ocjena > 1
+    AND
+    ispit.sifNastavnik NOT IN (SELECT sifNastavnik FROM ispit WHERE ocjena > 1 AND mbrStud != stud.mbrStud);

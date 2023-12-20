482593b1d921c2a4eee10b034fce5f9c00d7735e
Elnur Bjelić <elnur.bjelic1337@gmail.com>
2023-12-20 15:13:50 +0100

    Dodan zadatak 2A zadace 10

--- RI207 - Baze podataka/Zadaća 10/zadaca10.sql
+++ RI207 - Baze podataka/Zadaća 10/zadaca10.sql
@@ -15,3 +15,45 @@ WHERE ocjObrane(diplom.mbrStud, datPrijava) > 1;
 
 SELECT AVG(ocjObrane(mbrStud, datPrijava)) FROM diplom
 WHERE ocjObrane(mbrStud, datPrijava) > 1;
+
+
+DELIMITER //
+CREATE FUNCTION prosjekOcjena()
+RETURNS DECIMAL(3, 2) DETERMINISTIC
+BEGIN
+	DECLARE prosOc DECIMAL(3, 2);
+
+	IF @counter IS NULL THEN
+		SET @counter = 0;
+	END IF;
+    SET @counter = @counter + 1;
+    IF @counter = 1 THEN
+		SELECT AVG(ocjena) INTO prosOc FROM ispit WHERE ocjena > 1;
+	ELSE
+		SET prosOc = NULL;
+	END IF;
+    RETURN prosOc;
+END//
+
+SELECT prosjekOcjena();
+
+DELIMITER //
+
+CREATE FUNCTION brojNepristup()
+RETURNS INTEGER DETERMINISTIC
+BEGIN
+	DECLARE brNep INTEGER;
+    
+    IF @counter1 IS NULL THEN
+		SET @counter1 = 0;
+	END IF;
+    SET @counter1 = @counter1 + 1;
+    IF @counter1 = 1 THEN
+		SELECT COUNT(*) INTO brNep FROM stud WHERE mbrStud NOT IN (SELECT DISTINCT mbrStud FROM ispit);
+	ELSE
+		SET brNep = NULL;
+	END IF;
+    RETURN brNep;
+END//
+
+SELECT brojNepristup();

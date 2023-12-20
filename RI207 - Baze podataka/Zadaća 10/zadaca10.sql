27ceeaf3836671559f27adefd32344f6985a7005
Elnur Bjelić <elnur.bjelic1337@gmail.com>
2023-12-20 15:12:32 +0100

    Dodan zadatak 1B zadace 10

--- RI207 - Baze podataka/Zadaća 10/zadaca10.sql
+++ RI207 - Baze podataka/Zadaća 10/zadaca10.sql
@@ -3,3 +3,12 @@ SELECT
     datPrijava,
 	ocjObrane(mbrStud, datPrijava) AS ukupnaOcjena
 FROM diplom;
+
+SELECT
+	diplom.mbrStud,
+    prezStud,
+    imeStud,
+    ocjObrane(diplom.mbrStud, datPrijava) AS ukupnaOcjena
+FROM diplom
+JOIN stud ON diplom.mbrStud = stud.mbrStud
+WHERE ocjObrane(diplom.mbrStud, datPrijava) > 1;

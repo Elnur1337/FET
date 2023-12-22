a4916b01bef713bf57ee35457eb6611919ce97c5
Elnur Bjelić <elnur.bjelic1337@gmail.com>
2023-12-22 12:20:52 +0100

    Created generic getValidNumberInput function

--- RI202 - Objektno orijentirano programiranje/Zadatak 1/getValidNumberInput.h
+++ RI202 - Objektno orijentirano programiranje/Zadatak 1/getValidNumberInput.h
@@ -1,3 +1,21 @@
 #pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 template <typename T>
-T getValidNumberInput();
+T getValidNumberInput() {
+	string input;
+	do {
+		cout << "Unos: ";
+		cin >> input;
+		try {
+			T option = stoi(input);
+			return option;
+		}
+		catch (const invalid_argument& err) {
+			cout << "Niste unijeli broj, molimo pokusajte ponovo!\n";
+		}
+	} while (true);
+}

1835db560194eddd617f59ee3c44501a29e140dc
Elnur Bjelić <elnur.bjelic1337@gmail.com>
2023-12-24 19:24:59 +0100

    Created Complex.h file

---
+++ RI202 - Objektno orijentirano programiranje/Zadatak 4/Complex.h
@@ -0,0 +1,24 @@
+#pragma once
+
+
+class Complex {
+private:
+	int x = 0;
+	int y = 0;
+
+public:
+	Complex();
+	Complex(int, int);
+
+	int getReal() const;
+	int getImag() const;
+
+	Complex operator+(const Complex& sec) const;
+	Complex operator-(const Complex& sec) const;
+	Complex operator*(const Complex& sec) const;
+	Complex operator/(const Complex& sec) const;
+	Complex& operator+=(const Complex& sec);
+	Complex& operator-=(const Complex& sec);
+	Complex& operator*=(const Complex& sec);
+	Complex& operator/=(const Complex& sec);
+};
